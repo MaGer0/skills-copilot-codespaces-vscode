728055f0eefb3fddcc336a7690992020f061e5d0
Mathew Geraldo PR <147039305+MaGer0@users.noreply.github.com>
2025-02-12 13:40:46 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+  return {
+    addSkill: function() {
+      console.log('Skill added');
+    },
+    removeSkill: function() {
+      console.log('Skill removed');
+    }
+  };
+}
